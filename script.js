34673f201dee7c4aa5dece0dfe541348970cb8f7
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-16 19:32:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const getImages = function (url) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = url;
+    img.addEventListener("load", function () {
+      resolve(img);
+    });
+    img.addEventListener("error", function () {
+      reject(`Failed to load image's URL: ${url}`);
+    });
+  });
+};
+
+btn.addEventListener("click", () => {
+  Promise.all([
+    getImages(images[0].url),
+    getImages(images[1].url),
+    getImages(images[2].url),
+  ])
+    .then((values) => {
+      values.forEach((image) => {
+        output.insertAdjacentElement("afterbegin", image);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
